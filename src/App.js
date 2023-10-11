51f042ede6ab26129a0451300159650a7a66b54c
Maryam-coding <maryamnoor97@gmail.com>
2023-10-10 23:03:59 -0700

    added the outline of the weather app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
         <h1>Weather App</h1>
 
         <footer>
           <small>This was open-source code by Maryam Noor Hussain on Github <a href="https://www.github.com/Maryam-coding" target="_blank">
             </a> </small>
           </footer>
+     </div>
     </div>
   );
 }
